b3768f0cf105eb654e7a830db5a36fe1176d3255
Glitch (florentine-storm-hospital) <none>
2021-12-09 19:29:29 +0000

    🕷🐁 Checkpoint ./routers/userrouter.js:39277724/27 ./views/home.ejs:39277724/247

--- routers/userrouter.js
+++ routers/userrouter.js
@@ -160,7 +160,7 @@ const userInfo = {
   referrence1_contact : referrence1_contact ,
   bank_name : req.body.bank_name ,
   account_holder_name : account_holder_name ,
-  ifscCode : ifsc_code,
+  ifscCode : req.body.ifsc_code,
   gender : gender,
   account_number : account_number,
   document_id : documnet_id,

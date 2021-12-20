135ac08152c92d34a7ee0679c42968d275794d58
Glitch (florentine-storm-hospital) <none>
2021-12-20 19:35:31 +0000

    🐙🚛 Checkpoint ./routers/userrouter.js:39277724/584

--- routers/userrouter.js
+++ routers/userrouter.js
@@ -606,7 +606,17 @@ user_gender.forEach(async item=> {
   
 let u = await Usermodel.find();
   u.forEach(async item => {
-   let tt = await Usermodel.find({$add : [ '$user_city_points', '$user_age_points' , '$user_gender_points' ]});
+   let tt = await Usermodel.aggregate(
+       [
+         { $set: {
+        user_total_points : '2'
+     }
+         },
+     {
+       $project:
+     {'result' :{ $sum : [ '$user_city_points', '$user_age_points' , '$user_gender_points' ]}}
+          }
+   ]);
    console.log(tt)
   })
   
